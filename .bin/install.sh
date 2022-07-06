ae5c2800812b2daf526837864af89d9c519700f7
YuSzmiya <skyebell13rei@gmail.com>
2022-07-06 09:39:53 +0900

    moved install.sh

--- .bin/install.sh
+++ .bin/install.sh
@@ -11,15 +11,15 @@ echo "start setup..."
 
 for f in * .*;
 do
-    [[ "$f" == ".git" ]] && continue
-    [[ "$f" == ".gitignore" ]] && continue
-    [[ "$f" == "README.md" ]] && continue
-    [[ "$f" == "install.sh" ]] && continue
-    [[ "$f" == "LICENSE" ]] && continue
-    [[ "$f" == "wallpaper-images" ]] && continue
-    [[ "$f" == "." ]] && continue
-    [[ "$f" == ".." ]] && continue
-    [[ "$f" == ".bin" ]] && continue
+     [[ "$f" == ".git" ]] && continue
+     [[ "$f" == ".gitignore" ]] && continue
+     [[ "$f" == "README.md" ]] && continue
+     [[ "$f" == "install.sh" ]] && continue
+     [[ "$f" == "LICENSE" ]] && continue
+     [[ "$f" == "wallpaper-images" ]] && continue
+     [[ "$f" == "." ]] && continue
+     [[ "$f" == ".." ]] && continue
+     [[ "$f" == ".bin" ]] && continue
 
     ln -snf ${THIS_DIR}/"${f}" $HOME/"${f}"
     echo "Installed $f"
